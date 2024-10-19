d4d634404ad479411a8fadccad52cc24d8600582
tirucloud <137248129+tirucloud@users.noreply.github.com>
2024-10-19 13:44:05 +0530

    Create ubuntu-gui-script.sh

---
+++ ubuntu-gui-script.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+sudo apt update
+sudo apt install ubuntu-desktop -y
+sudo apt install tightvncserver -y
+sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+vncserver
+sudo apt install lxde -y
+sudo apt install xrdp -y
+sudo passwd ubuntu
